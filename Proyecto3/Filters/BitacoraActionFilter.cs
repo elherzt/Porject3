4b805d15903a66890601b8b61469b43b63b191c0
Jesús Romero <elherzt@gmail.com>
2014-02-25 11:45:40 -0600

    modified controller and filter to record actions

--- Proyecto3/Filters/BitacoraActionFilter.cs
+++ Proyecto3/Filters/BitacoraActionFilter.cs
@@ -10,23 +10,24 @@ namespace Proyecto3.Filters
     public class BitacoraActionFilter : ActionFilterAttribute, IActionFilter
     {
 
-        void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
+        void IActionFilter.OnActionExecuted(ActionExecutedContext filterContext)
         {
         pruebaDataContext prueba = new pruebaDataContext();
         HttpSessionStateBase session = filterContext.HttpContext.Session;
         var id = Convert.ToInt32(session["id"]);
+        var mensaje = filterContext.Controller.TempData["Accion"].ToString() + filterContext.Controller.TempData["Datos"].ToString();
         PRUEBAS_TA_05_BITACORA log = new PRUEBAS_TA_05_BITACORA()
         {
             T_CONTROLLER = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
             T_METHOD = filterContext.ActionDescriptor.ActionName,
             E_USER_ID = id,
             F_DATE = filterContext.HttpContext.Timestamp,
-            T_MESSAGE = "A dummy message"
+            T_MESSAGE = mensaje
         };
 
         prueba.PRUEBAS_TA_05_BITACORA.InsertOnSubmit(log);
         prueba.SubmitChanges();
-        this.OnActionExecuting(filterContext);
+        this.OnActionExecuted(filterContext);
     }
 
     }
